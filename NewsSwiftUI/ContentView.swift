0be9d22ad3e157ef8c6478b53040b6d21f2bddca
Kjatata <kuskill777@yandex.by>
2021-01-21 11:26:22 +0300

    Initial Commit

---
+++ NewsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsSwiftUI
+//
+//  Created by User on 21.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsSwiftUI/NewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIApp.swift
+//  NewsSwiftUI
+//
+//  Created by User on 21.01.21.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
