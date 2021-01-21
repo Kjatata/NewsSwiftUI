19aa0f01d1122fb9ea2662429370360bbff6a29e
Kjatata <kuskill777@yandex.by>
2021-01-21 11:55:21 +0300

    Try to get API

--- NewsSwiftUI/ContentView.swift
+++ NewsSwiftUI/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Get") {
+            guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=51ea6b34f2be4e8b819ba235b717cf44") else { return }
+            let session = URLSession.shared
+            session.dataTask(with: url) { (data, response, error ) in
+                if let response = response {
+                    print(response)
+                }
+                
+                guard let data = data else { return }
+                print(data)
+                
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }.resume()
+        }
     }
 }
 
