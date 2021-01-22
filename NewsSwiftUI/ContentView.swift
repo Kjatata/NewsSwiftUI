98520c6722638c5be8e2f61257848c374ef1d5e1
Kjatata <kuskill777@yandex.by>
2021-01-22 13:26:50 +0300

    add structs

--- NewsSwiftUI/ContentView.swift
+++ NewsSwiftUI/ContentView.swift
@@ -7,22 +7,37 @@
 
 import SwiftUI
 
+struct NewsArticles: Decodable {
+    var articles: [Article]
+}
+
+struct Article: Decodable {
+    var author: String
+    var description: String
+    var title: String
+    var url: String
+    var urlToImage: String
+    var publishedAt: String
+    var source: Source?
+}
+
+struct Source: Decodable {
+    var name: String
+}
+
 struct ContentView: View {
     var body: some View {
         Button("Get") {
             guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=51ea6b34f2be4e8b819ba235b717cf44") else { return }
-            let session = URLSession.shared
-            session.dataTask(with: url) { (data, response, error ) in
-                if let response = response {
-                    print(response)
-                }
+            URLSession.shared.dataTask(with: url) { (data, _, error ) in
                 
                 guard let data = data else { return }
-                print(data)
                 
                 do {
                     let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print(json)
+                    let jsonT = try JSONDecoder().decode(NewsArticles.self, from: data)
+                    print(jsonT)
+                    //print(json)
                 } catch {
                     print(error)
                 }
